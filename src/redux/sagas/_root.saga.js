dc8187d8441d1d795712ee4933c2895d6916b768
Jamie Anne White <jimmy.anne.white@gmail.com>
2020-12-04 14:01:41 -0600

    client delete

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -1,9 +1,10 @@
 import { all } from "redux-saga/effects";
 import petsSaga from "./pets.saga.js";
+import deletePetsSaga from "./delete.pets.saga"
 
 export default function* rootSaga() {
   console.log('in root saga')
   yield all([
-      petsSaga(),
+      petsSaga(), deletePetsSaga()
   ]);
 }
